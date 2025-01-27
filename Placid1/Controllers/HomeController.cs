bb7c1ef3063669d505494e3081ef60c9764402f6
dfrazzitta <dfrazzitta@gmail.com>
2025-01-26 19:08:48 -0500

    Add project files.

---
+++ Placid1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Placid1.Models;
+
+namespace Placid1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IWebHostEnvironment _env;
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env; 
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
